eb3df7c0239ea5187be40e6db2b22036c30da306
HoangHieuHUIT <hoanghieucity1@gmail.com>
2024-01-09 16:02:53 +0700

    first commit

---
+++ API/FarmProductionAPI.Core/Commands/UserSiteCommand/CancleOrderCommand.cs
@@ -0,0 +1,11 @@
+﻿using FarmProductionAPI.Domain.Dtos;
+using FarmProductionAPI.Domain.Response;
+
+namespace FarmProductionAPI.Core.Commands.UserSiteCommand
+{
+    public class CancleOrderCommand : ICommand<ResponseResultAPI<OrderDTO>>
+    {
+        public Guid? Id { get; set; }
+        public string? Reason { get; set; }
+    }
+}
---
+++ API/FarmProductionAPI.Domain/Enums/StatusOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProductionAPI.Domain.Enums
+{
+    public enum StatusOrder
+    {
+        Pending,
+        Delivering,
+        Acepted,
+        Cancle,
+        Completed,
+        WaitingCancle
+    }
+}
